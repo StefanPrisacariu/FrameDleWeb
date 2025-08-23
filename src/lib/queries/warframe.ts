946d1fe7f0089af9ab8becfea5083077882209e9
Stefan Prisacariu <stefan.prisacariu@qonditive.com>
2025-08-23 10:12:05 +0300

    no message

--- src/lib/queries/warframe.ts
+++ src/lib/queries/warframe.ts
@@ -1,15 +1,15 @@
-import { initialWarframes } from '@/app/lib/warframes';
-import axios from 'axios';
+import { initialWarframes } from "@/app/lib/warframes";
+import axios from "axios";
 
-const REACT_APP_TODAY_ENDPOINT = process.env.REACT_APP_TODAY_ENDPOINT || 'http://localhost:4000';
-const REACT_APP_YESTERDAY_ENDPOINT = process.env.REACT_APP_YESTERDAY_ENDPOINT || 'http://localhost:4000';
+const REACT_APP_TODAY_ENDPOINT = process.env.REACT_APP_TODAY_ENDPOINT;
+const REACT_APP_YESTERDAY_ENDPOINT = process.env.REACT_APP_YESTERDAY_ENDPOINT;
 
 export const fetchTodaysWarframe = async (): Promise<Warframe> => {
-    const res = await axios.get(REACT_APP_TODAY_ENDPOINT);
+    const res = await axios.get(REACT_APP_TODAY_ENDPOINT as string);
     return initialWarframes[res.data.number] as Warframe;
 };
 
 export const fetchYesterdayWarframe = async (): Promise<Warframe> => {
-    const res = await axios.get(REACT_APP_YESTERDAY_ENDPOINT);
+    const res = await axios.get(REACT_APP_YESTERDAY_ENDPOINT as string);
     return initialWarframes[res.data.number] as Warframe;
 };
