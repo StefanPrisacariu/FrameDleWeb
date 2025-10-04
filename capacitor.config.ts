c515db10c951ce201f46cce2e7cdf66be4da9fb3
Leokai Skarri <>
2025-10-04 20:42:08 +0300

    web bullshit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "com.framedle",
+    appName: "FrameDle",
+    webDir: "out",
+};
+
+export default config;
